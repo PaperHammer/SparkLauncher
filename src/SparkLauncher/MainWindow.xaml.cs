110add88b7af30e57e1514fbc7a35e98ad3c0e1c
PaperHammer <paperhammer@outlook.com>
2025-01-06 23:58:58 +0800

    Reapply "feat: 初步规划"
    This reverts commit 3b7efcab0500a09f90035b1f8ef67495c2d49085.

--- src/SparkLauncher/MainWindow.xaml.cs
+++ src/SparkLauncher/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SparkLauncher.Common.Utils.PInvoke;
+using WinRT.Interop;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,13 +15,112 @@ namespace SparkLauncher {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window {
+    public sealed partial class MainWindow : WindowEx {
+        public SolidColorBrush WindowCaptionForeground => (SolidColorBrush)App.Current.Resources["WindowCaptionForeground"];
+        public SolidColorBrush WindowCaptionForegroundDisabled => (SolidColorBrush)App.Current.Resources["WindowCaptionForegroundDisabled"];
+
         public MainWindow() {
             this.InitializeComponent();
+
+            SetWindowStyle();
+            SetWindowTitleBar();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e) {
-            myButton.Content = "Clicked";
+        private void WindowEx_Activated(object _, WindowActivatedEventArgs args) {
+            if (args.WindowActivationState == WindowActivationState.Deactivated) {
+                TitleTextBlock.Foreground = WindowCaptionForegroundDisabled;
+            }
+            else {
+                TitleTextBlock.Foreground = WindowCaptionForeground;
+            }
         }
+
+        private void SetWindowStyle() {
+            this.SystemBackdrop = new MicaBackdrop();
+        }
+
+        #region window title bar
+        private void SetWindowTitleBar() {
+            //ref: https://learn.microsoft.com/en-us/windows/apps/develop/title-bar?tabs=wasdk
+            if (AppWindowTitleBar.IsCustomizationSupported()) {
+                var titleBar = this.AppWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                titleBar.ButtonForegroundColor = WindowCaptionForeground.Color;
+
+                AppTitleBar.Loaded += AppTitleBar_Loaded;
+                AppTitleBar.SizeChanged += AppTitleBar_SizeChanged;
+            }
+            else {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AppTitleBar_Loaded(object _, RoutedEventArgs e) {
+            if (AppWindowTitleBar.IsCustomizationSupported()) {
+                SetDragRegionForCustomTitleBar(this.AppWindow);
+            }
+        }
+
+        private void AppTitleBar_SizeChanged(object _, SizeChangedEventArgs e) {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && this.AppWindow.TitleBar.ExtendsContentIntoTitleBar) {
+                // Update drag region if the size of the title bar changes.
+                SetDragRegionForCustomTitleBar(this.AppWindow);
+            }
+        }
+
+        private void SetDragRegionForCustomTitleBar(AppWindow appWindow) {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && appWindow.TitleBar.ExtendsContentIntoTitleBar) {
+                double scaleAdjustment = GetScaleAdjustment();
+
+                RightPaddingColumn.Width = new GridLength(appWindow.TitleBar.RightInset / scaleAdjustment);
+                LeftPaddingColumn.Width = new GridLength(appWindow.TitleBar.LeftInset / scaleAdjustment);
+
+                List<Windows.Graphics.RectInt32> dragRectsList = [];
+
+                Windows.Graphics.RectInt32 dragRectL;
+                dragRectL.X = (int)((LeftPaddingColumn.ActualWidth) * scaleAdjustment);
+                dragRectL.Y = 0;
+                dragRectL.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectL.Width = (int)((IconColumn.ActualWidth
+                                        + TitleColumn.ActualWidth
+                                        + LeftDragColumn.ActualWidth) * scaleAdjustment);
+                dragRectsList.Add(dragRectL);
+
+                Windows.Graphics.RectInt32 dragRectR;
+                dragRectR.X = (int)((LeftPaddingColumn.ActualWidth
+                                    + IconColumn.ActualWidth
+                                    + TitleColumn.ActualWidth
+                                    + LeftDragColumn.ActualWidth) * scaleAdjustment);
+                dragRectR.Y = 0;
+                dragRectR.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectR.Width = (int)(RightDragColumn.ActualWidth * scaleAdjustment);
+                dragRectsList.Add(dragRectR);
+
+                Windows.Graphics.RectInt32[] dragRects = dragRectsList.ToArray();
+
+                appWindow.TitleBar.SetDragRectangles(dragRects);
+            }
+        }
+
+        private double GetScaleAdjustment() {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            DisplayArea displayArea = DisplayArea.GetFromWindowId(wndId, DisplayAreaFallback.Primary);
+            IntPtr hMonitor = Win32Interop.GetMonitorFromDisplayId(displayArea.DisplayId);
+
+            // Get DPI.
+            int result = Native.GetDpiForMonitor(hMonitor, Native.Monitor_DPI_Type.MDT_Default, out uint dpiX, out uint _);
+            if (result != 0) {
+                throw new Exception("Could not get DPI for monitor.");
+            }
+
+            uint scaleFactorPercent = (uint)(((long)dpiX * 100 + (96 >> 1)) / 96);
+            return scaleFactorPercent / 100.0;
+        }
+        #endregion
     }
 }
