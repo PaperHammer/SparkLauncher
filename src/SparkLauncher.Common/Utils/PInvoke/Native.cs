f910e1663116c7fc2c44cdce4096627a5a8cd559
PaperHammer <paperhammer@outlook.com>
2025-01-06 23:57:18 +0800

    feat: 初步规划

---
+++ src/SparkLauncher.Common/Utils/PInvoke/Native.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SparkLauncher.Common.Utils.PInvoke {
+    public static class Native {
+        [DllImport("Shcore.dll", SetLastError = true)]
+        public static extern int GetDpiForMonitor(IntPtr hmonitor, Monitor_DPI_Type dpiType, out uint dpiX, out uint dpiY);
+
+        public enum Monitor_DPI_Type : int {
+            MDT_Effective_DPI = 0,
+            MDT_Angular_DPI = 1,
+            MDT_Raw_DPI = 2,
+            MDT_Default = MDT_Effective_DPI
+        }
+    }
+}
---
+++ src/SparkLauncher.Models/ResponsibilityChain/Base/ResponsibilityHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace SparkLauncher.Common.Core {
+    public abstract class ResponsibilityHandler {
+        public void LinkIn(ResponsibilityHandler next) => _next = next;
+        public ResponsibilityHandler? GetNextHandler() => _next;
+        public abstract void Handle();
+
+        private ResponsibilityHandler? _next;
+    }
+}
